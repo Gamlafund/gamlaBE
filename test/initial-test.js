1a67f363a8788129591538cab61c3f50d9ce87e7
stephane <stephane@paperlib.com>
2021-12-01 23:29:11 +0100

    add test for balance + addapt/deprecate numberOfParticipants check

--- test/initial-test.js
+++ test/initial-test.js
@@ -5,23 +5,33 @@ const { ethers } = require("hardhat");
 
 describe("Community Fund", function () {
   let communityFundFactory;
+  let communityFund;
+  let participants;
 
   before( async ()=>{
     const CommunityFundFactory = await ethers.getContractFactory("CommunityFundFactory");
     communityFundFactory = await CommunityFundFactory.deploy();
     await communityFundFactory.deployed();
-  })
-  it("Create a Community Fund", async  ()=> {
-    let accounts = await ethers.getSigners();
 
-    const deployCommunityFund = await communityFundFactory.createCommunityFund(10, 1000, 12, 1638314000, [accounts[0].address])
+    participants = (await ethers.getSigners()).map(account => account.address);
+  })
+
+  it("Should create a Community Fund", async  ()=> {
+    const deployCommunityFund = await communityFundFactory.createCommunityFund(participants, 1000, 12, 1638314000)
     const CommunityFund = await ethers.getContractFactory("CommunityFund");
 
-    const communityFund = CommunityFund.attach((await deployCommunityFund.wait()).events[0].args.communityFundAddress);
+    communityFund = CommunityFund.attach((await deployCommunityFund.wait()).events[0].args.communityFundAddress);
+
     let   communityFundData = await communityFund.getCommunityFundData();
 
-    expect(await communityFundData.numberOfParticipants.toNumber() == 10);
+    expect(await communityFundData.participants.length == participants.length);
     expect(await communityFundData.recurringAmount.toNumber() == 1000);
     expect(await communityFundData.duration.toNumber() == 12);
   });
+
+  it("Should have 0 balance at first", async  ()=> {
+    expect( (await hre.ethers.provider.getBalance(communityFund.address)).toBigInt() == 0);
+  });
+
+  // TODO: deposit amount on the community fund contract and verify the contract balances matches it.
 });
