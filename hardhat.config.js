4c09423ef544843262dece678b066ff857474344
stephane <stephane@paperlib.com>
2021-12-09 20:19:49 +0100

    include etherscan and dotenv config updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// -- sample hardhat task: cf. https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+  for (const account of accounts) { console.log(account.address); }
 });
 
 // You need to export an object to set up your config
@@ -17,5 +15,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4"
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_RINKEBY_URL,
+      accounts: [process.env.RINKEBY_ACCOUNT_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
